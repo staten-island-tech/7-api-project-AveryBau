5ad74a962e2c54763d229c6d59528f25bda267f3
AveryBau <averya34@nycstudents.net>
2026-04-24 10:17:13 -0400

    Update API.py

--- API.py
+++ API.py
@@ -1,14 +1,19 @@
 import requests
 import json
 
-def getPoke(poke):
-    response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{poke.lower()}")
+def getFish(fish):
+    response = requests.get(f"https://www.fisheries.noaa.gov/topic/sustainable-seafood{fish.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
     print(data)
+    types = []
+    for t in data["types"]:
+        types.append(t["type"]["name"])
+    for key, value in seafood.items():
+        print(key, "→", value)
     return {
         "name": data["name"],
         "height": data["height"],
@@ -16,5 +21,8 @@ def getPoke(poke):
         "types": [t["type"]["name"] for t in data["types"]]
     }
 
-pokemon = getPoke("Bulbasaur")
-print(pokemon)
+seafood = getFish("Alaska Snow Crab")
+print(seafood)
+for key, value in seafood.items():
+    print(f"{key.title()}: {value}")
+
