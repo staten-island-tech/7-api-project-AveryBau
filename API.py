436219f33e45614f83a88e29f27ea326e97f8a32
AveryBau <averya34@nycstudents.net>
2026-04-28 09:55:56 -0400

    Update API.py

--- API.py
+++ API.py
@@ -2,21 +2,22 @@ import requests
 import json
 
 def getMeow(cat):
-    response = requests.get(f"https://meowfacts.herokuapp.com/{cat.lower()}")
-    if response.status_code != 100:
+    response = requests.get(f"https://meowfacts.herokuapp.com/")
+    if response.status_code != 200:
         print("Error fetching data!")
         return None
     
     data = response.json()
     print(data)
-    return {
-        "name": data["name"],
-        "height": data["height"],
-        "weight": data["weight"],
-        "types": [t["type"]["name"] for t in data["types"]]
-    }
 
-fact = getMeow("")
+""" {
+    "data": [
+    "0": "Mother cats teach their kittens to use the litter box.",
+    "1": "A cat can sprint at about thirty-one miles per hour."
+    ]
+} """
+
+fact = getMeow
 print(fact)
 
 """ def divide(a,b):
