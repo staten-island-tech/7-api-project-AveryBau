ea7a59bfd62984780c144b41cc890bb362ed041b
AveryBau <averya34@nycstudents.net>
2026-04-30 10:17:15 -0400

    Update API.py

--- API.py
+++ API.py
@@ -24,17 +24,19 @@ print(fact) """
 fact = getmultipleMeow("3")
 print(fact) """
 
-
-""" def getspecificMeow(cat):
+print("HELLO!!! DO YOU WANT TO KNOW FACTZ ABOUT CATZ???")
+def getspecificMeow(cat):
     response = requests.get(f"https://meowfacts.herokuapp.com/?id={cat.lower()}")
     if response.status_code != 200:
         print("Error fetching data!")
         return None
     data = response.json()
     print(data)
-
+ask = int(input("What fact do ya wanna learn today? Pick a random number."))
 fact = getspecificMeow("1")
-print(fact) """
+print(fact)
+
+
 
 
 """ def getlanguageMeow(cat):
