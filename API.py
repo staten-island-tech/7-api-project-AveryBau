5cdd1c2b3d56ba1c787e1fe56b7e8935f29441f4
AveryBau <averya34@nycstudents.net>
2026-05-19 10:01:17 -0400

    Update API.py

--- API.py
+++ API.py
@@ -1,87 +1,34 @@
 import requests
-import json
-import random
-import tkinter
 from tkinter import *
 import tkinter as tk 
 
-""" def getrandomMeow(cat):
-    response = requests.get(f"https://meowfacts.herokuapp.com/{cat.lower()}")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    data = response.json()
-    print(data)
+def getMeow():
+    Fact = Enter.get
+    get = requests.get(f"https://meowfacts.herokuapp.com/?id={Fact()}")
+    if get.status_code != 200:
+        ResponseFact.config(text = "Error fetching data!")
+    else:
+        data = get.json()
+        ResponseFact.config(text=f"Fact:{data["data"]}", bg = "pink", fg="black")
 
-fact = getrandomMeow("")
-"""
-
-
-""" def getmultipleMeow(cat):
-    response = requests.get(f"https://meowfacts.herokuapp.com/?count={cat.lower()}")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    data = response.json()
-    print(data)
-
-fact = getmultipleMeow("3")"""
-
-
-
-
-
-
-print("Hello! Do you wanna learn factz about catz?")
-Userinput = input("What do you want to learn? Type any number.")
-print(f"You chose fact number {Userinput}")
-
-def meow_fact():
-    text = entry.get()
-    result_label.config(text)
-    return
-    
-def getspecificMeow(cat):
-    response = requests.get(f"https://meowfacts.herokuapp.com/?id={Userinput}")
-    if response.status_code != 200:
-        print("Error fetching data!")
-        return None
-    data = response.json()
-    print(data)
-getspecificMeow(Userinput)
     
 window = tk.Tk()
-window.geometry("700x700")
+window.geometry("1000x1000")
 window.title("Meow Facts")
 window.resizable(True, True)
 
 
-prompt = tk.Label(window, text="Hello! Do you wanna learn factz about catz? Type any number below:", font=("Arial", 14))
-prompt.pack(pady=10)
+prompt = tk.Label(window, text="Hello! Do you wana learn factz about catz? Type any number below", font=("Arial", 20))
+prompt.pack(pady=20, padx=10)
 
-entry = tk.Entry(window, font=("Arial", 14), width = 30)
-entry.pack(pady=5)
+Enter = tk.Entry(window, font=("Arial", 20))
+Enter.pack(pady=20, padx=10)
 
-result_label = tk.Label(window, text="", font=("Arial", 14, "bold"), fg="blue")
-result_label.pack(pady=15)
+ResponseFact = tk.Label(window, text="", font=("Arial", 20), anchor='center', wraplength=700, justify="center")
+ResponseFact.pack(pady=20, padx=10)
 
-button = tk.Button(window, text="Enter", font=("Arial", 14), command=meow_fact) 
-button.pack(pady=10)
+button = tk.Button(window, text="Enter", font=("Arial", 20), command=getMeow) 
+button.pack(pady=10, padx=10)
 
 window.mainloop()
 
-""" Title = Label(master = Window, text = "Meow Facts")
-Title.pack()
-Enter = Entry(master = Window)
-Enter.pack
-
-instruct = Label(master = Window, text = "What do you want to learn? Type any number.")
-instruct.pack
-
-Checked = Label(master = Window, text = "check")
-Checked.pack 
-
-Button1 = Button(master = Window, command = facts, text = "Type any number.")
-Button1.pack
-
-newMeow() """
